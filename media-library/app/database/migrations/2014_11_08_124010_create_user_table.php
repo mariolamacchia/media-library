3bd8cbf344edc6ac496c5d9083f3e5dc4cf37780
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-08 13:54:37 +0100

    migrate: create users table

---
+++ media-library/app/database/migrations/2014_11_08_124010_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    Schema::create('users', function($table)
+    {
+      $table->string('username')->primary();
+      $table->string('folder');
+      $table->boolean('admin')->default(false);
+      $table->char('password', 60);
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    Schema::drop('users');
+	}
+
+}
